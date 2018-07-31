bfc55852ae1936b444b3d9a6fb79433076a01bd2
Koji Kawabata <14ec043@ms.dendai.ac.jp>
2018-07-31 16:31:15 +0900

    これがメイン関数

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+'''
+Created on 2018/07/10
+
+@author: 皓司
+'''
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def main():
+    ani=animation.
+
+
+if __name__ == '__main__':
+    main()
